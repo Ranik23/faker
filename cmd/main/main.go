d4639716265f16af098f84b23f5f552c0e74b50a
anton <antonfedorov190@yandex.ru>
2024-11-16 22:26:37 +0300

    changed

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,27 +3,12 @@ package main
 import (
 	"faker/internal/db"
 	"faker/internal/seeds"
-	"fmt"
 	"log"
-	"os"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		dbHost, dbUser, dbPassword, dbName, dbPort)
 
-	dbConn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
+	dbConn := db.Connect()
 
 	db.Migrate(dbConn)
 
