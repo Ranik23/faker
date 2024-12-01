41824c5d7197af267fc90cff27341d2e4725ab64
anton <antonfedorov190@yandex.ru>
2024-12-01 08:30:11 +0300

    updated

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -10,33 +10,33 @@ func main() {
 
 	dbConn := db.Connect()
 
-	// db.Migrate(dbConn)
+	db.Migrate(dbConn)
 
-	// for i := 0; i < 10000; i++ {
-	// 	seeds.CreateCustomer(dbConn)
-	// }
+	for i := 0; i < 10000; i++ {
+		seeds.CreateCustomer(dbConn)
+	}
 
-	// for i := 0; i < 400; i++ {
-	// 	seeds.CreateAddress(dbConn)
-	// }
+	for i := 0; i < 400; i++ {
+		seeds.CreateAddress(dbConn)
+	}
 
-	// for i := 0; i < 10000; i++ {
-	// 	seeds.CreatePostOffice(dbConn)
-	// }
+	for i := 0; i < 10000; i++ {
+		seeds.CreatePostOffice(dbConn)
+	}
 
-	// seeds.CreatePostalStatus(dbConn)
+	seeds.CreatePostalStatus(dbConn)
 
-	// for i := 0; i < 10000; i++ {
-	// 	seeds.CreateEmployee(dbConn)
-	// }
+	for i := 0; i < 10000; i++ {
+		seeds.CreateEmployee(dbConn)
+	}
 
-	// for i := 0; i < 10000; i++ {
-	// 	seeds.CreatePostalItem(dbConn)
-	// }
+	for i := 0; i < 10000; i++ {
+		seeds.CreatePostalItem(dbConn)
+	}
 
-	// for i := 0; i < 10000; i++ {
-	// 	seeds.CreatePayment(dbConn)
-	// }
+	for i := 0; i < 10000; i++ {
+		seeds.CreatePayment(dbConn)
+	}
 
 	for i := 0; i < 10000; i++ {
 		seeds.CreateStatusTransaction(dbConn)
